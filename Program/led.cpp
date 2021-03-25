d59c6fb9f32a28d75332740202ef6c2c48460806
GarrettOHara <garrettohara2018@gmail.com>
2021-03-25 16:21:21 -0700

    Implemented CLI arguments with error handling. Read multiple files with fstream library. Created source code file from example in project

--- Program/led.cpp
+++ Program/led.cpp
@@ -7,16 +7,48 @@
  * CS 530 | Lenoard
 **/
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
+int readFile(const char* input){
+    std::ifstream file(input);
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            printf("%s", line.c_str());
+        }
+        printf("\n");
+    }
+    file.close();
+    return 0;
+}
+
 int main(int argc, char *argv[]){
-    if(argc < 1){
-        cout << "Error: Please specify a listing file to parse" << endl;
-    } else if(argc >  3){
-        cout << "Error: Please provide 1-3 listing files to parse" << endl;
+    printf("The size of argc is: %d\n",argc);
+    if(argc < 2){
+        cout << "User Error: Please specify a listing file to parse" << endl;
+        return 0;
+    } else if(argc > 4){
+        cout << "Error: Please provide 1-4 listing files to parse" << endl;
+        return 0;
     }
 
     for(int i = 0; i < argc; i++){
-        cout << argv[i] << endl;
+        readFile(argv[i]);
     }
+    return 0;
 }
+
+/**
+ * This is a notes section
+ * 
+ * ELF/ in the output is the text file header, is holds entry addresses 
+ * which is the first address in a .text region
+ * use objdump-f to see the enty point
+ * 
+ * from: https://stackoverflow.com/questions/3102232/elf-file-format
+ * 
+ * 
+ * 
+ **/
